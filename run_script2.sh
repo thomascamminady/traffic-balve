6e5fcb3078724aca38dcaa4b6cc14cce4ff5dc3d
Thomas Camminady <0milieux_member@icloud.com>
2024-01-09 07:52:30 +0100

    Update.

--- run_script2.sh
+++ run_script2.sh
@@ -2,11 +2,12 @@
 
 # Navigate to the script's directory
 cd /Users/thomascamminady/Repos/traffic_balve
+LOG_FILE=/Users/thomascamminady/Repos/traffic_balve/cron.log
 
 echo "##########################################################GIT#################################################################" >> $LOG_FILE
 # Log the current date and time
-CURRENT_TIMESTAMP=$(date)
+# CURRENT_TIMESTAMP=$(date)
 # Commit the changes
-GIT_COMMIT_MSG="Adding data at date $CURRENT_TIMESTAMP."
-git commit -am "$GIT_COMMIT_MSG"
+# GIT_COMMIT_MSG="Adding data at date $CURRENT_TIMESTAMP."
+# git commit -am "$GIT_COMMIT_MSG"
 git push
