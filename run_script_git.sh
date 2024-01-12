51dd4f7be4e158572e2922fcc23a3544125fee4d
Thomas Camminady <0milieux_member@icloud.com>
2024-01-12 09:18:23 +0100

    Adding run git.

---
+++ run_script_git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the path to the poetry executable and your script
+POETRY_PATH=/Users/thomascamminady/.local/bin/poetry
+
+# Define the log file location
+LOG_FILE=/Users/thomascamminady/Repos/traffic_balve/cron.log
+
+# Navigate to the script's directory
+cd /Users/thomascamminady/Repos/traffic_balve
+
+echo "####################################################################################################################################" >> $LOG_FILE
+# Log the current date and time
+CURRENT_TIMESTAMP=$(date)
+
+CURRENT_TIMESTAMP=$(date)
+GIT_COMMIT_MSG="Adding data at date $CURRENT_TIMESTAMP."
+
+$POETRY_PATH run git commit -am "$GIT_COMMIT_MSG"
+$POETRY_PATH run git push
