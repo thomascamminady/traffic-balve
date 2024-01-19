48f47d9cc9090ebbeafadc02cf89e8fed89636e4
Thomas Camminady <0milieux_member@icloud.com>
2024-01-19 08:26:25 +0100

    Updating scripts.

--- run_git_push.sh
+++ run_git_push.sh
@@ -1,3 +1,22 @@
+
+
 #!/bin/bash
-cd /Users/thomascamminady/Repos/traffic_balve
-git push
+
+REPO_DIR="/Users/thomascamminadywahoo/Repos/traffic_balve"
+LOG_FILE="/Users/thomascamminadywahoo/Repos/traffic_balve/.gitpush.log"
+GIT_PATH="/opt/homebrew/bin/git"
+# Start the log
+echo "Starting git push at $(date)" >> "$LOG_FILE"
+
+# Change into the repository directory
+cd "$REPO_DIR" || exit
+
+# Execute git push and log the output
+{
+    echo "Changing into directory: $(pwd)"
+    echo "Running git push..."
+    $GIT_PATH push
+} >> "$LOG_FILE" 2>&1
+
+# End the log
+echo "Git push completed at $(date)" >> "$LOG_FILE"
