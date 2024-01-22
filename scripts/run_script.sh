0e3297a872b4b3baaa2b39bbe20f714f1f6d69bf
Thomas Camminady <0milieux_member@icloud.com>
2024-01-22 14:43:56 +0100

    Adding data at date Mon Jan 22 14:43:56 CET 2024.

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -22,5 +22,5 @@ $POETRY_PATH run python $SCRIPT_PATH >> $LOG_FILE 2>&1
 CURRENT_TIMESTAMP=$(date)
 GIT_COMMIT_MSG="Adding data at date $CURRENT_TIMESTAMP."
 
-$POETRY_PATH run git commit -am "$GIT_COMMIT_MSG"
+$POETRY_PATH run git commit -am "$GIT_COMMIT_MSG" >> $LOG_FILE 2>&1
 git push >> $LOG_FILE 2>&1
